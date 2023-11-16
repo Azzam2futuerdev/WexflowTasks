2f40116acd8f7ec8a40856a4d76703fc9035859a
Maksym Zinchenko <siqsuruq@gmail.com>
2023-11-16 15:35:39 -0100

    Wexflow.Tasks.FlipCoin
    FlipCoin puts in Log random "Heads" or "Tail"

---
+++ Wexflow.Tasks.FlipCoin/FlipCoin.cs
@@ -0,0 +1,23 @@
+﻿using Wexflow.Core;
+using Task= Wexflow.Core.Task;
+using TaskStatus = Wexflow.Core.TaskStatus;
+using System.Xml.Linq;
+
+namespace Wexflow.Tasks.FlipCoin
+{
+    public class FlipCoin :Task
+    {
+        public FlipCoin(XElement xe, Workflow wf) : base(xe, wf)
+        {
+        }
+
+        public override TaskStatus Run()
+        {
+            Info("Flipping coin...");
+            var result = new Random().Next(0, 2) == 0 ? "Heads" : "Tails";
+            InfoFormat("Result: {0}", result);
+            return new TaskStatus(Status.Success);
+        }
+
+    }
+}
