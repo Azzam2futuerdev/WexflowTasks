4969c015ee76d9da603f3b4d0a5b553fcfc8e995
Maksym Zinchenko <siqsuruq@gmail.com>
2024-05-08 22:51:09 -0100

    Get basic info from Docker API
    Get basic info from Docker API: name, containers running, stopped or paused and version

--- Wexflow.Tasks.Docker/Wexflow.Tasks.DockerVersion/Tasks.DockerVersion.cs
+++ Wexflow.Tasks.Docker/Wexflow.Tasks.DockerVersion/Tasks.DockerVersion.cs
@@ -3,6 +3,7 @@ using Wexflow.Core;
 using Task = Wexflow.Core.Task;
 using TaskStatus = Wexflow.Core.TaskStatus;
 using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace Wexflow.Tasks.DockerVersion
 {
@@ -16,7 +17,7 @@ namespace Wexflow.Tasks.DockerVersion
         public DockerVersion(XElement xe, Workflow wf) : base(xe, wf)
         {
             DockerHost = GetSetting("dockerHost");
-            DockerPort = GetSettingInt("dockerPort", 2376); // Default to 2376 for TLS
+            DockerPort = GetSettingInt("dockerPort", 2375); // Default to 2376 for TLS
             DockerCertPath = GetSetting("dockerCertPath");
             DockerTlsVerify = GetSetting("dockerTlsVerify");
         }
@@ -29,7 +30,15 @@ namespace Wexflow.Tasks.DockerVersion
             {
                 using (HttpClient client = new HttpClient())
                 {
-                    client.BaseAddress = new Uri($"https://{DockerHost}:{DockerPort}/");
+                    if ( DockerPort == 2375)
+                    {
+                        client.BaseAddress = new Uri($"http://{DockerHost}:{DockerPort}/");
+                    } 
+                    else
+                    {
+                        client.BaseAddress = new Uri($"https://{DockerHost}:{DockerPort}/");
+                    }
+
                     client.DefaultRequestHeaders.Accept.Clear();
                     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
@@ -40,6 +49,36 @@ namespace Wexflow.Tasks.DockerVersion
                     {
                         var result = response.Content.ReadAsStringAsync().Result;
                         Info("Docker info retrieved.");
+                        // Parse the JSON response
+                        var jsonObject = JObject.Parse(result);
+						
+						// Name
+                        string name = jsonObject["Name"].Value<int>();
+                        Info($"Name: {name}");
+
+                        // Number of Containers
+                        int containers = jsonObject["Containers"].Value<int>();
+                        Info($"Number of Containers: {containers}");
+						
+						// Number of ContainersRunning
+                        int rcontainers = jsonObject["ContainersRunning"].Value<int>();
+                        Info($"Number of Running Containers: {rcontainers}");
+
+						// Number of ContainersPaused
+                        int pcontainers = jsonObject["ContainersPaused"].Value<int>();
+                        Info($"Number of Running Containers: {pcontainers}");
+						
+						// Number of ContainersStopped
+                        int scontainers = jsonObject["ContainersStopped"].Value<int>();
+                        Info($"Number of Running Containers: {scontainers}");
+						
+                        // Add more fields as needed
+                        int images = jsonObject["Images"].Value<int>();
+                        Info($"Number of Images: {images}");
+
+                        string dockerVersion = jsonObject["ServerVersion"].Value<string>();
+                        Info($"Docker Server Version: {dockerVersion}");
+
                         return new TaskStatus(Status.Success, false);
                     }
                     else
